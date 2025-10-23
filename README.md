d905d43333ce2bc8ac5f4ec1fefd1c1a93a9c7cf
Himanshu Rani <hrs.1510@gmail.com>
2025-10-23 11:33:12 +0200

    movies data fetching code is restructured into clean code

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This web app is 'Movie Browser' with the help of using https://developer.themoviedb.org/docs/getting-started OPEN API
 This app is created using plain JS and CSS.
+this app is divided into 3 man sections:Header, Main and Footer.
+Header has movie browser title, navigation bar for filtering and search input to search movies by their title.
+
+Main section has movie cards which are created dynamically with JS.
+To run this app, you need to get your own API key from https://www.themoviedb.org/settings/api.
+this data dosn't include movie posters. it just has poster path property, so you need to get it from image url using poster path query.
+
+Footer has social media links and copyright information.
 
 
 
--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTYxNzM5ZTIxZjJjYTlkY2EzNWY4NDQ3NjEzMTY5ZiIsIm5iZiI6MTc2MDY0Njk0NC4zOTYsInN1YiI6IjY4ZjE1NzIwYmZjNTZlYmFjNTFlMjMyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4uKFLTWJMHZEnexIkFDSOgxEcWPrvLdTuK3hleMOzk0'
-  }
-};
- //popular movies
-fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+const API_KEY = '2a61739e21f2ca9dca35f8447613169f';  // Api key for themoviedb generated from account
+const BASE_URL = "https://api.themoviedb.org/3";      // Base URL for The Popular Movies Database API
+const Poster_URL = 'https://image.tmdb.org/t/p/w500'; // Base URL for movie posters
 
+
+
+//popular movies
+const movie_url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;//url created for popular movies for batter syntax
+const movieData = async () => {
+  const response = await fetch(movie_url);
+  const data = await response.json();
+  console.log(data);
+}
+movieData();
 /*
 //top rated movies
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
   .then(res => res.json())
   .then(res => console.log(res))
   .catch(err => console.error(err));
+*!/
 */
